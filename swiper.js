e4d9989563212f66fd30e4908a825b9d190ccd35
EURent24 <138871348+EURent24@users.noreply.github.com>
2023-07-12 14:55:18 +0200

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -3,12 +3,20 @@ window.addEventListener("load", function (){
     const swipe = 799
 
     document.querySelector("#button-next").addEventListener('click',function (){
+        document.querySelectorAll(".swiper-slide").forEach(slide =>{
+            const length = slide.style.transform.replace('translate3d(', '').split(',')[0].replace('px', '')
+            slide.style.transform = `translate3d(${parseInt(length) - swipe}px ,0,0)`
+        })
         const length = wrapper.style.transform.replace('translate3d(', '').split(',')[0].replace('px', '')
-        wrapper.style.transform = `translate3d(${parseInt(length) - swipe} ,0,0)`
+        wrapper.style.transform = `translate3d(${parseInt(length) - swipe}px ,0,0)`
     })
     
     document.querySelector("#button-prev").addEventListener('click',function (){
+        document.querySelectorAll(".swiper-slide").forEach(slide =>{
+            const length = slide.style.transform.replace('translate3d(', '').split(',')[0].replace('px', '')
+            slide.style.transform = `translate3d(${parseInt(length) + swipe}px ,0,0)`
+        })
         const length = wrapper.style.transform.replace('translate3d(', '').split(',')[0].replace('px', '')
-        wrapper.style.transform = `translate3d(${parseInt(length) + swipe} ,0,0)`
+        wrapper.style.transform = `translate3d(${parseInt(length) + swipe}px ,0,0)`
     })
 })
