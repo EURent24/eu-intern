29f4e2d6335a3251b2651495f3a6b1b019839b39
EURent24 <138871348+EURent24@users.noreply.github.com>
2023-07-12 13:55:56 +0200

    Update functions.js

--- _next/static/js/functions.js
+++ _next/static/js/functions.js
@@ -84,3 +84,27 @@ drift.load(drift_id);
     b.onload = a;
     document.getElementsByTagName("head")[0].appendChild(b)
 })();
+
+(function () {
+    var a = document.createElement("script");
+    a.type = "text/javascript";
+    a.async = !0;
+    a.src = ("https:" == document.location.protocol ? "https://" : "http://") + "t.sf14g.com/sf14g.js";
+    var c = document.getElementsByTagName("script")[0];
+    c.parentNode.insertBefore(a, c)
+})();
+(function () {
+    function a() {
+        !1 === c && (c = !0, Munchkin.init("306-ZEJ-256"))
+    }
+
+    var c = !1, b = document.createElement("script");
+    b.type = "text/javascript";
+    b.async = !0;
+    b.src = "//munchkin.marketo.net/munchkin.js";
+    b.onreadystatechange = function () {
+        "complete" != this.readyState && "loaded" != this.readyState || a()
+    };
+    b.onload = a;
+    document.getElementsByTagName("head")[0].appendChild(b)
+})();
